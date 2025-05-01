acfde5db9883f66d37254fe42ad0c3bb8eb3d671
Gazi2050 <gazi45344@gmail.com>
2025-05-01 18:49:33 +0600

    error page added

--- tasko-frontend/src/Components/ErrorComponent.tsx
+++ tasko-frontend/src/Components/ErrorComponent.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
 import { errorImg } from '../Constants/data';
+import { Link } from 'react-router';
 
 const ErrorComponent = () => {
     return (
-        <div>
-            <img src={errorImg} alt="" />
+        <div className="min-h-screen flex items-center justify-center bg-white px-4">
+            <div className="bg-white border border-gray-200 p-10 md:p-16 rounded-3xl shadow-2xl text-center w-full max-w-4xl">
+                <img
+                    src={errorImg}
+                    alt="Error"
+                    className="mx-auto mb-8  object-contain"
+                />
+                <Link
+                    to="/"
+                    className="inline-block bg-[#60E5AE] text-black font-semibold px-8 py-3 rounded-xl shadow hover:bg-[#4cd3a0] transition"
+                >
+                    Back To Home
+                </Link>
+            </div>
         </div>
     );
 };
